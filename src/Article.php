8546340de3454860f8308110a3905083760bb722
jerichen <jerichen@cw.com.tw>
2018-09-13 00:05:50 +0800

    dev packages test

---
+++ src/Article.php
@@ -0,0 +1,15 @@
+<?php
+namespace Jerichen\Amp; #namespace 會跟 composer.json定義一樣
+
+use DOMDocument;
+use Imagick;
+use Log;
+use Exception;
+
+class Article
+{
+    public function result()
+    {
+        return 'Hello world!';
+    }
+}
